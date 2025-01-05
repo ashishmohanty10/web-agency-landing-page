33bf17414d19d10bc4291a3f6458028ea80b2bbc
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-01-05 18:06:58 +0530

    feat: add initial pricing comp

---
+++ src/components/icons/addmore-icon.tsx
@@ -0,0 +1,18 @@
+export function AddMoreIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      focusable="false"
+      fill="none"
+      className={className}
+    >
+      <g>
+        <path
+          d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"
+          fill="currentColor"
+        ></path>
+      </g>
+    </svg>
+  );
+}
---
+++ src/components/sections/pricing-section.tsx
@@ -0,0 +1,163 @@
+import { CircleDollarSign } from "lucide-react";
+import { Container } from "../container-wrapper";
+import { FeatureHighlight } from "../title";
+import { pricing } from "@/lib/constant";
+import { cn } from "@/lib/utils";
+import { FramerDesign } from "../icons/framer-design-icon";
+import { Badge } from "../badge";
+import { AddMoreIcon } from "../icons/addmore-icon";
+import { Button } from "../button";
+import Image from "next/image";
+
+export function Pricing() {
+  return (
+    <>
+      <Container className="py-10">
+        <div className="flex items-center flex-col justify-center space-y-5">
+          <FeatureHighlight>
+            <div className="w-15 h-[.5px] bg-brand"></div>
+            <CircleDollarSign />
+            <p>Plans & Pricing</p>
+          </FeatureHighlight>
+
+          <div>
+            <p className="text-8xl font-semibold text-black leading-tight text-center">
+              Transparent Pricing,
+              <br />
+              <span className="text-secondary-title">
+                No Surprises Guaranteed{" "}
+              </span>
+            </p>
+          </div>
+
+          <div className="flex justify-center">
+            <p className="text-lg text-text text-center w-[70%]">
+              We offer comprehensive web design and development services,
+              including custom solutions, e-commerce platforms, responsive
+              designs, and maintenance, as well as digital marketing, SEO, and
+              IT consulting services.
+            </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 py-6 gap-8">
+          {pricing.map((item, idx) => (
+            <div
+              key={idx}
+              className="w-full border border-black/10 rounded-2xl p-4 h-full"
+            >
+              <div className="p-5 rounded-3xl bg-brand/10 w-fit mb-5">
+                <item.icon className="w-7 h-7 text-brand" />
+              </div>
+
+              <div className="flex flex-col justify-evenly items-stretch ">
+                <div className="space-y-4 border-b border-black/10">
+                  <p className="text-sm text-brand font-semibold">
+                    {item.plan}
+                  </p>
+                  <p
+                    className={cn(
+                      "text-xs text-text font-medium",
+                      idx === 2 && "text-4xl font-semibold text-black"
+                    )}
+                  >
+                    {item.description}
+                  </p>
+
+                  <div className="py-4">
+                    <p className="text-4xl font-semibold">
+                      {item.price}
+                      <span className="text-md font-medium">/month</span>
+                    </p>
+                  </div>
+                </div>
+
+                <div className="py-5 space-y-4">
+                  {item.features.map((data) => (
+                    <div
+                      key={data.label}
+                      className={cn(
+                        "flex items-center gap-4 w-full",
+                        idx === 2 && "text-7xl font-semibold text-text"
+                      )}
+                    >
+                      <data.icon className="w-7 h-7 text-text" />
+                      <p className="text-lg text-text">{data.label}</p>
+                    </div>
+                  ))}
+                </div>
+
+                <div>
+                  {item.addOn ? (
+                    <div className="flex items-center justify-between gap-2 bg-brand/10 rounded-3xl px-2 py-1">
+                      <div className="flex items-center  gap-2">
+                        <FramerDesign className="text-brand w-5 h-5" />
+                        <p className="text-sm text-brand font-medium">
+                          {item.addOn.title} {item.addOn.price}
+                        </p>
+                      </div>
+                      <Badge className="flex items-center gap-1 bg-gradient-to-tr from-brand to-brand/10">
+                        <AddMoreIcon className="w-2 h-2 text-white" />
+                        Add
+                      </Badge>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+
+                <div
+                  className={cn(
+                    "flex justify-center pt-5",
+                    idx === 2 && "pt-[10rem]"
+                  )}
+                >
+                  <Button size="large" className="shadow-md shadow-brand/70">
+                    Get Started Today
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="flex w-full gap-10 ">
+          <div className="w-1/2 p-4 border border-black/10 rounded-3xl">
+            <div className="flex items-center gap-2 mb-5">
+              <div className="border border-black/10 rounded-full overflow-hidden w-fit">
+                <Image
+                  src="/profile.avif"
+                  alt="profile"
+                  width={60}
+                  height={60}
+                />
+              </div>
+              <p className="text-3xl text-black font-semibold">Ashley Hall</p>
+            </div>
+
+            <p className="text-xl font-medium">
+              &quot;Ripple Template is a game-changer - fast, flexible,
+              flawless, and utterly transformed my workflow.&quot;
+            </p>
+          </div>
+
+          <div className="w-1/2 p-4 bg-black rounded-3xl flex flex-col justify-between">
+            <p className="text-3xl font-semibold text-white leading-normal">
+              Still Can&apos;t Decide? <br />
+              <span className="text-secondary-title">
+                Let Us Help you Make the Right Decision.
+              </span>
+            </p>
+
+            <div className="space-x-4 ">
+              <Button size="small">Send Us an Email</Button>
+              <Button size="small" variant="secondary">
+                Schedule a call
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
